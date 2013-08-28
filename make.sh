de149105ea0fbe14b9009e8dcbbb908ecfdda4bc
lsb <leebutterman@gmail.com>
2013-08-28 17:22:34 +0000

    add breadcrumbs and factor out permalinking

--- make.sh
+++ make.sh
@@ -1,16 +1,34 @@
 #!/bin/bash
+function permalink {
+    base=`basename $1`
+    name=${base%.*}
+    spacedname=`echo $name | tr - ' '`
+    echo "<p class=permalink><a href='${base}' name='${name}'>${spacedname}</a></p>"
+}
+function crumb {
+    if [ $2 ]
+    then 
+	echo "<div class=${1}crumb>"
+	permalink $2
+	echo "</div>"
+    fi
+}
 echo "<!DOCTYPE html><html><head><title>"
 cat assets/blog-title
 echo "</title><style type='text/css'>"
 cat assets/css
 echo "</style></head><body>"
 cat assets/frontispiece
+crumb next $NEXTLINK
+crumb prev $PREVLINK
+crumb up $UPLINK
+
 for f in "$@"
 do
     echo "<div class=post><p class=timestamp>"
     date --date="`stat -c %y $f`" +"%Y-%m-%d %H:%M %Z"
     echo "</p>"
-    echo "<p class=permalink><a href='`basename $f`.html'>`basename $f|tr '-' ' '`</a></p>"
+    permalink ${f}.html
     cat $f
     echo "</div><hr>"
 done
